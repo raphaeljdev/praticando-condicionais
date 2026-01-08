ee6432ae24823f5e9cd3bd1c85f3229de5d5f745
raphael <raphaelj.dev@gmail.com>
2026-01-08 19:35:41 -0300

    Adiciona estrutura do exercicio acesso-liberado

---
+++ acesso-liberado.js
@@ -0,0 +1,3 @@
+// Você está desenvolvendo um sistema de controle de acesso para um evento. Para entrar, a pessoa precisa ter 18 anos ou mais.
+
+// Crie um programa que, dado o valor da idade, verifique se a pessoa pode ou não entrar no evento. Exiba a mensagem informando se o acesso foi ou não permitido.
